f2777afd80f6ef84d5b49c2b24fb30f9955d959e
Ben <85500722+Ben-Bussch@users.noreply.github.com>
2025-09-25 00:41:50 +0100

    Started implementation of proper fire sequence code

--- Avionics/COMET-I-test/src/main.cpp
+++ Avionics/COMET-I-test/src/main.cpp
@@ -4,36 +4,15 @@
 #include "SD.h"
 #include <Wire.h>
 #include <PT.cpp>
+#include <EC.cpp>
 //#include <SoftwareSerial.h>
 
-//Servos
-Servo NoxEngServo;
-Servo IPAEngServo;
-Servo FillServo;
-int NoxEngPin = 6;
-int IPAEngPin = 9;
-int FillPin = 10;
+int fireSeq = 0;
+int fillSeq = 0;
 
-int NoxEngStartPPM = 1050;
-int IPAEngStartPPM = 1400;
-int FillStartPPM = 1050;
-
-
-int NoxdeltaPPM = 800;
-int IPAdeltaPPM = 600;
-int FilldeltaPPM = 750;
-
-int PPMpos = 0; 
-int count = 0;
-
-//Control
-int PyroPin = 21;
-int FillSequPin = 36;
-int FirePin = 34;
 int clk_time = 0;
-
-
-
+int FireStartTime = 0;
+int launchtime = 0;
 //SD
 const int mchipSelect = BUILTIN_SDCARD;
 
@@ -52,56 +31,16 @@ void setup()
 
   String PT_status = SetupCurrentSensor();
   Serial.println(PT_status);
-  
-  //Servos
-  NoxEngServo.attach(NoxEngPin);
-  NoxEngServo.writeMicroseconds(NoxEngStartPPM); 
 
-  IPAEngServo.attach(IPAEngPin);
-  IPAEngServo.writeMicroseconds(IPAEngStartPPM); 
-
-  FillServo.attach(FillPin);
-  FillServo.writeMicroseconds(FillStartPPM); 
-
-  //Inputs and Outputs
-  pinMode(PyroPin, OUTPUT);
-
-  pinMode(FirePin, INPUT);
-  pinMode(FillSequPin,  INPUT);
-
-  
+  SetupControl();  
 }
 
-void fireSequence()
-{
-  FillServo.writeMicroseconds(FillStartPPM);
-  delay(5000); 
-  NoxEngServo.writeMicroseconds(NoxEngStartPPM+NoxdeltaPPM);
-  delay(100); 
-  IPAEngServo.writeMicroseconds(IPAEngStartPPM+IPAdeltaPPM);
 
-}
-
-void fillSequence()
-{
-
-  NoxEngServo.writeMicroseconds(NoxEngStartPPM);
-  IPAEngServo.writeMicroseconds(IPAEngStartPPM);
-  
-  FillServo.writeMicroseconds(FillStartPPM);
-  for (PPMpos = 0; PPMpos <= (FilldeltaPPM); PPMpos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 ppm
-    FillServo.writeMicroseconds(PPMpos+FillStartPPM);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 5ms for the servo to reach the position
-  }
-
-}
 
 
 void loop()
 {
   clk_time = millis();
-  
 
   float pressure = ReadPressureTransducer();
   if(pressure > p_max){
@@ -116,44 +55,30 @@ void loop()
   }
 
 
-  if (digitalRead(FillSequPin) == HIGH){
+  if (digitalRead(FillSequPin) == LOW ){
+    Serial.println("Fill is LOW");
+  }
+
+  if (digitalRead(FillSequPin) == HIGH && digitalRead(FirePin) == LOW){
     Serial.println("Fill is HIGH");
   }
 
-  if (digitalRead(FirePin) == HIGH){
-    Serial.println("Fire is HIGH");
-    digitalWrite(PyroPin, HIGH);
-  }
-
+  
   if (digitalRead(FirePin) == LOW){
-    Serial.println("Fire is LOW");
+    //Serial.println("Fire is LOW");
     digitalWrite(PyroPin, LOW);
+    fireSeq = 0;
+    FireStartTime = clk_time;
   }
-  
-    
-    
-  
 
-
-  
-  //Serial.println(p_max);
-
-  //Serial.println("Hello from Teensy 4.1!");
-  
-    /*
-    if(count < 1){
-    delay(1000);
-    fillSequence();
-    //FillServo.writeMicroseconds(FillStartPPM);
-    delay(20000);
-    fireSequence();
-    //FillServo.writeMicroseconds(FillStartPPM+FilldeltaPPM);
-    
-    count ++;
-   
+  if (digitalRead(FirePin) == HIGH){
+    //Serial.println("Fire is HIGH");
+    launchtime = fireSequence(FireStartTime, clk_time, fireSeq);
+    if(launchtime%1000 == 0){
+        Serial.println(launchtime);
+    }
   }
-  */
-  
+
 
 }
 
